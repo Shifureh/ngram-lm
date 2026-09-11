b504262a569268f0fba84dc2e73de5a2c775aa00
UTahseen <tahseenu@nycstudents.net>
2026-09-11 17:45:16 -0400

    Updated schema to include n-gram length

--- build_ngrams.py
+++ build_ngrams.py
@@ -15,17 +15,17 @@ def init_db(db_path="ngrams.db", schema_path="schema.sql"):
     return conn
 
 
-def save_ngrams(conn, ngram_counts):
+def save_ngrams(n, conn, ngram_counts):
     records = [
-        (" ".join(ngram[:-1]), ngram[-1], count)
+        (n, " ".join(ngram[:-1]), ngram[-1], count)
         for ngram, count in ngram_counts.items()
     ]
     cursor = conn.cursor()
     cursor.executemany(
         """
-        INSERT INTO ngrams (context, next_word, count)
-        VALUES (?, ?, ?)
-        ON CONFLICT(context, next_word) 
+        INSERT INTO ngrams (n_length, context, next_word, count)
+        VALUES (?, ?, ?, ?)
+        ON CONFLICT(n_length,context, next_word) 
         DO UPDATE SET count = count + excluded.count
         """,
         records,
@@ -36,16 +36,15 @@ def save_ngrams(conn, ngram_counts):
 def main():
     raw_texts = load_all_texts("data")
     tokenized_texts = tokenize_all(raw_texts)
-
     conn = init_db("ngrams.db", "schema.sql")
-
+    # for n in range(2,7):
     n = 2
 
     for filepath, tokens in tokenized_texts.items():
         print(f"Processing {filepath}...")
         ngrams = generate_ngrams(tokens, n=n)
         counts = Counter(ngrams)
-        save_ngrams(conn, counts)
+        save_ngrams(n, conn, counts)
 
     print("n-grams inserted successfully")
 
@@ -61,12 +60,32 @@ def main():
         SELECT context, next_word, count 
         FROM ngrams 
         ORDER BY count DESC 
-        LIMIT 5;
+        LIMIT 1;
         """
     )
-    print("\n5 most frequent bigrams:")
+    print("\nMost frequent bigrams:")
     for context, next_word, count in cursor.fetchall():
         print(f"'{context}' -> '{next_word}': {count}")
+
+    cursor.execute(
+        """
+        SELECT *
+        FROM ngrams
+        """
+    )
+    print("\nAll bigrams:")
+    for n_length, context, next_word, count in cursor.fetchall():
+        print(f"{n_length}: '{context}' -> '{next_word}': {count}")
+
+    cursor.execute(
+        """
+        SELECT SUM(COUNT) FROM ngrams;
+        """
+    )
+
+    total_count = cursor.fetchone()[0]
+    print(f"\nTotal count of all bigrams: {total_count}")
+
 
     conn.close()
 
